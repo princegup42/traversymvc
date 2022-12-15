844eccd326bf74905f3a1dc556b95552e7e2c165
princegup42 <guptasum521@gmail.com>
2022-12-15 19:15:49 +0530

    old code clean

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// App Root
-define('APPROOT', dirname(dirname(__FILE__)));
-
-// URL Root
-define('URLROOT', 'http://localhost/traversymvc');
-
-// Site Name
-define('SITENAME', 'TraversyMVC');
--- app/views/inc/footer.php
+++
@@ -1,4 +0,0 @@
-<script src="<?php echo URLROOT ?>/public/js/main.js"></script>
-</body>
-
-</html>
