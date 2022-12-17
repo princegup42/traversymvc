38835334668c26c1cfe9f428babfd7b6ca7e3b6b
princegup42 <guptasum521@gmail.com>
2022-12-17 22:35:12 +0530

    Header & Footer Includes

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -12,7 +12,9 @@ class Pages extends Controller {
     }
 
     public function about() {
-        // echo $id;
-        $this->view( 'pages/about' );
+        $data = [
+            'title' => 'Welcome About Page',
+        ];
+        $this->view( 'pages/about', $data );
     }
 }
