a24a6f0d23635fed1cb443abb119a1fb3dcdb024
princegup42 <guptasum521@gmail.com>
2022-12-29 22:40:07 +0530

    Form Validation

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -19,7 +19,6 @@ $password_err = ( !empty( $data['password_err'] ) ) ? 'is-invalid' : '';
                     <input type="password" name="password" class="form-control form-control-lg <?php echo $password_err; ?>" value="<?php echo $data['password']; ?>">
                     <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
                 </div>
-
                 <div class="row">
                     <div class="col">
                         <input type="submit" value="Login" class="btn btn-success btn-block">
