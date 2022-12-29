7e2f57ef545234d80f0a3f20e28bd660d43c12a1
princegup42 <guptasum521@gmail.com>
2022-12-29 21:42:07 +0530

    Creating The Users Controller

---
+++ app/controllers/Users.php
@@ -0,0 +1,29 @@
+<?php
+
+class Users extends Controller {
+    # code...
+    public function __construct() {
+        # code...
+    }
+
+    public function register() {
+        //Check for POST
+        if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+            //Process form
+        } else {
+            //Init data
+            $data = [
+                'name'                 => '',
+                'email'                => '',
+                'password'             => '',
+                'confirm_password'     => '',
+                'name_err'             => '',
+                'email_err'            => '',
+                'password_err'         => '',
+                'confirm_password_err' => '',
+            ];
+
+            $this->view( 'users/register', $data );
+        }
+    }
+}
