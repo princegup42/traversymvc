847df0b261407c27ecbce970354dfe98def520cf
princegup42 <guptasum521@gmail.com>
2023-01-02 22:38:38 +0530

    User Session Data & Logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -131,7 +131,7 @@ class Users extends Controller {
                 $loggedInuser = $this->userModel->login( $data['email'], $data['password'] );
                 if ( $loggedInuser ) {
                     // Create Session
-                    die( 'success' );
+                    $this->createUserSession( $loggedInuser );
                 } else {
                     $data['password_err'] = 'Password Incorrect';
                     $this->view( 'users/login', $data );
@@ -151,6 +151,30 @@ class Users extends Controller {
 
             // Load view
             $this->view( 'users/login', $data );
+        }
+    }
+
+    public function createUserSession( $user ) {
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->name;
+
+        redirect( 'pages/index' );
+    }
+
+    public function logout() {
+        unset( $_SESSION['user_id'] );
+        unset( $_SESSION['user_email'] );
+        unset( $_SESSION['user_name'] );
+        session_destroy();
+        redirect( 'users/login' );
+    }
+
+    public function isLoggedIn() {
+        if ( isset( $_SESSION['user_id'] ) ) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
